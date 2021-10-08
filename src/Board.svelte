3c3267fa11de75ab632559255b23dc3ed659393f
yaikohi <erik.beem@student.hu.nl>
2021-10-08 10:56:30 +0200

    add pawn bool toggle

--- src/Board.svelte
+++ src/Board.svelte
@@ -1,115 +1,115 @@
-<script>
+<script lang="ts">
     import Square from "./Square.svelte";
-    let clrList = ["beige", "darkgrey"];
+
+    let clrList: string[] = ["beige", "darkgrey"];
+    let sz: number = 50;
 </script>
 
 <main>
     <div class="container">
         <div class="board">
             <div class="row">
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
             </div>
             <div class="row">
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
             </div>
             <div class="row">
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
             </div>
             <div class="row">
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
             </div>
             <div class="row">
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
             </div>
             <div class="row">
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
-                <Square sqrClr={clrList[1]} sqrSize="50" />
-                <Square sqrClr={clrList[0]} sqrSize="50" />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} />
             </div>
             <div class="row">
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
             </div>
             <div class="row">
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[1]} sqrSize="50" hasPwn="true" />
-                <Square sqrClr={clrList[0]} sqrSize="50" hasPwn="true" />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[1]} sqrSize={sz} hasPwn={true} />
+                <Square sqrClr={clrList[0]} sqrSize={sz} hasPwn={true} />
             </div>
         </div>
     </div>
 </main>
 
-<!-- markup (zero or more items) goes here -->
 <style>
     .container {
-        padding: 50px;
         display: flex;
         justify-content: center;
     }
--- src/Square.svelte
+++ src/Square.svelte
@@ -1,27 +1,19 @@
 <script lang="ts">
     import Pawn from "./Pawn.svelte";
-    import type {
-        Square as SquareT,
-        Pawn as PawnT,
-    } from "./interfaces/checkers";
-
     export let sqrClr: string;
     export let sqrSize: number;
     export let hasPwn: boolean = false;
 
-    // let createSqr = (color: string, size: string, haspwn: boolean): SquareT => {
-    //     let sqr: SquareT = {
-    //         color: color,
-    //         area: { width: size, height: size },
-    //         hasPawn: haspwn,
-    //     };
-    //     return sqr;
-    // };
+    function modPawn() {
+        hasPwn = !hasPwn;
+    }
+
 </script>
 
 <main>
     {#if hasPwn}
         <div
+            on:click|preventDefault={modPawn}
             class="sqr pwn sqr--container"
             style="
             background-color: {sqrClr};
@@ -33,6 +25,7 @@
         </div>
     {:else}
         <div
+            on:click|preventDefault={modPawn}
             class="sqr pwn--false"
             style="
             background-color: {sqrClr};
@@ -43,7 +36,6 @@
     {/if}
 </main>
 
-<!-- markup (zero or more items) goes here -->
 <style>
     .sqr {
         border: 0.5px solid rgba(130, 199, 255, 0.3);
